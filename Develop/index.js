d4c78f955a8560952a82ac9f5c355dbe28bf61c8
JRM <johnrhmartin@gmail.com>
2022-03-26 13:27:55 -0400

    added an array of questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,19 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {question: "What is your project title?"},
+    {question: "Why did you build this project?"},
+    {question: "What problem does it solve?"},
+    {question: "What did you learn?"},
+    {question: "How do you install your project?"},
+    {question: "What third-party assets were required to build this project?"}
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
+
 
 // TODO: Create a function to initialize app
 function init() {}
