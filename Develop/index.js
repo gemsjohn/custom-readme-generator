344df3247690d5c9d65c3f95e1bae10b3bac4471
JRM <johnrhmartin@gmail.com>
2022-03-26 13:41:05 -0400

    added six unique prompts, a then statement to handle the answers, and a catch for error handling

--- Develop/index.js
+++ Develop/index.js
@@ -11,12 +11,109 @@ const questions = [
     {question: "What third-party assets were required to build this project?"}
 ];
 
+let arr = [];
+
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 
 // TODO: Create a function to initialize app
-function init() {}
+const init = questionArray => {
+    if (!questionArray) {
+        questionArray = [];
+    }
+    return inquirer.prompt ([
+        {
+            type: 'input',
+            name: 'arr_0',
+            message: questions[0].question,
+            validate: projectTitleInput => {
+                if (projectTitleInput) {
+                    return true;
+                } else {
+                    console.log('Please provide a response!')
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'arr_1',
+            message: questions[1].question,
+            validate: projectTitleInput => {
+                if (projectTitleInput) {
+                    return true;
+                } else {
+                    console.log('Please provide a response!')
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'arr_2',
+            message: questions[2].question,
+            validate: projectTitleInput => {
+                if (projectTitleInput) {
+                    return true;
+                } else {
+                    console.log('Please provide a response!')
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'arr_3',
+            message: questions[3].question,
+            validate: projectTitleInput => {
+                if (projectTitleInput) {
+                    return true;
+                } else {
+                    console.log('Please provide a response!')
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'arr_4',
+            message: questions[4].question,
+            validate: projectTitleInput => {
+                if (projectTitleInput) {
+                    return true;
+                } else {
+                    console.log('Please provide a response!')
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'arr_5',
+            message: questions[5].question,
+            validate: projectTitleInput => {
+                if (projectTitleInput) {
+                    return true;
+                } else {
+                    console.log('Please provide a response!')
+                    return false;
+                }
+            }
+        }, 
+    ])
+    .then((answers) => {
+        console.log(answers);
+    })
+    .catch((error) => {
+        if (error.isTtyError) {
+            console.log("Prompt couldn't be rendered int he current environment");
+        } else {
+            console.log("Something else went wrong");
+        }
+    });
+}
 
 // Function call to initialize app
 init();
+
